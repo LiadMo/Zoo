2459e04ca8b7fabd955a96de8828121e7dab4ea0
Liad Motorin <liad.motorin@intel.com>
2022-08-08 18:07:40 +0300

    Add project files.

---
+++ Zoo/Canvas.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zoo
+{
+    internal class Canvas
+    {
+        private int Width;
+        private int Height;
+    }
+}
---
+++ Zoo/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Zoo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 9;
+            double y = x;
+            Console.WriteLine(x);
+        }
+    }
+}
