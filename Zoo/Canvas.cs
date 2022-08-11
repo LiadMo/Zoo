280a9916e2aebe1315e2cb81aa564a77da40efd9
Liad Motorin <liad.motorin@intel.com>
2022-08-11 09:57:36 +0300

    init Canvas class

--- Zoo/Canvas.cs
+++ Zoo/Canvas.cs
@@ -10,5 +10,27 @@ namespace Zoo
     {
         private int Width;
         private int Height;
+        private static Canvas instance = null; // hold the instance
+
+        // private c'tor (Singelton)
+        private Canvas(int width, int height)
+        {
+            Width = width;
+            Height = height;
+        }
+        
+        // call the c'tor only is the first instance
+        public Canvas CreateCanvas(int width = 600, int height = 200)
+        {
+            if (instance == null)
+                instance = new Canvas(width, height);
+            return instance;
+        }
+
+        public void DrawCanvas()
+        {
+            Console.Clear();
+            // toDO
+        }
     }
 }
